a41a771c0a6f8b2b14c4ad90d45b906b1d498fc7
noemiepham <huynhtieuquy@gmail.com>
2022-07-26 23:14:24 +0200

    modifier fichiers

--- starting_code/getline.c
+++ starting_code/getline.c
@@ -11,27 +11,18 @@ int main(void)
 	size_t line_buf_size = 0;
 	int line_count = 0;
 	ssize_t line_size;
-	line_buf = (char *)malloc(line_buf_size);
-	if (line_buf == NULL)
-		return (-1);
+
 	printf("$ ");
 
-	/* Get the first line of the file. */
+	/* Get the standard input line */
 	line_size = getline(&line_buf, &line_buf_size, stdin);
 
-	/* Loop through until we are done with the file. */
-	while (line_size >= 0)
+	if (line_size >= 0)
 	{
-		/* Increment our line count */
-		line_count++;
-
 		/* Show the line details */
 		printf("command: %s\n", line_buf);
 		printf("size: %zu\n", line_buf_size);
 		printf("Line_size: %zd\n", line_size);
-
-		/* Get the next line */
-		line_size = getline(&line_buf, &line_buf_size, stdin);
 	}
 	/* Free the allocated line buffer */
 	free(line_buf);
--- starting_code/strtok_split.c
+++ starting_code/strtok_split.c
@@ -4,25 +4,32 @@
 
 int main()
 {
-	char str[] = "strtok needs to be called several times to split a string";
-	int init_size = strlen(str);
+	ssize_t nread;
+	size_t len = 0;
+	char *line = NULL;
+
+	printf("$ ");
+
+	nread = getline(&line, &len, stdin);
+
+	int init_size = strlen(line);
 	char delim[] = " ";
 
-	char *ptr = strtok(str, delim);
+	char *ptr = strtok(line, delim);
 
 	while(ptr != NULL)
 	{
-		printf("'%s'\n", ptr);
-		ptr = strtok(NULL, delim);
-	}
+		printf("%s\n", ptr);
+ 		ptr = strtok(NULL, delim); 
+	} 
 
 	/* This loop will show that there are zeroes in the str after tokenizing */
 	for (int i = 0; i < init_size; i++)
 	{
-		printf("%d ", str[i]); /* Convert the character to integer, in this case
+		printf("%d ", line[i]); /*Convert the character to integer, in this case
 							   the character's ASCII equivalent */
 	}
-	printf("\n");
+	printf("\n"); 
 
 	return 0;
 }
