feefce5070bd3460d72f504459d404e174694f1b
noemiepham <huynhtieuquy@gmail.com>
2022-07-29 00:57:33 +0200

    update programe hsh.c

--- hsh.c
+++ hsh.c
@@ -9,13 +9,12 @@ int main(void)
     
     char *str = NULL;
 	size_t len = 0;
-    ssize_t line_cm;
     unsigned int i = 0;
     char **arr;
 
     printf("$ ");
 
-	line_cm = getline(&str, &len, stdin);
+	getline(&str, &len, stdin);
 
     arr = split_str(str);
 
