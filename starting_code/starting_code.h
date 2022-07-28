cf715607647e0417167e937254b9f380c3df3678
noemiepham <huynhtieuquy@gmail.com>
2022-07-28 21:56:58 +0200

    update code.h

--- starting_code/starting_code.h
+++ starting_code/starting_code.h
@@ -1,12 +1,17 @@
 #ifndef starting_code_h
 #define starting_code_h
 
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
-#include <sys/types.h>
+#include <unistd.h>
 #include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+char *_which(char *command, char *fullpath, char *path);
+char *_getenv(const char *name);
 
 /* Prortotypes */
 
