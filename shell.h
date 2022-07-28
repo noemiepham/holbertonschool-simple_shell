c9ed95d487c9efbf37d2e9f88f8f627c5240d92b
noemiepham <huynhtieuquy@gmail.com>
2022-07-28 23:42:35 +0200

    shell check out

---
+++ shell.h
@@ -0,0 +1,21 @@
+#ifndef starting_code_h
+#define starting_code_h
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+char *_which(char *command, char *fullpath, char *path);
+char *_getenv(const char *name);
+
+/* Prortotypes */
+
+char **split_str(char *str);
+pid_t _fork(void);
+
+#endif
