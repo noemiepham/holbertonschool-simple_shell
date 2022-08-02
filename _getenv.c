48236da5b1d40c59ee263a44f0693a7e6aa5a08c
noemiepham <huynhtieuquy@gmail.com>
2022-08-02 09:46:04 +0200

    status 1

---
+++ _getenv.c
@@ -0,0 +1,44 @@
+
+#include "shell.h"
+/**
+ * _getenv - get environ
+ * @name: name de environ
+ * Return: environ value
+ */
+
+char *_getenv(const char *name)
+{
+    int i = 0;
+    char *envName;
+    char *envValue;
+
+    while (environ[i])
+    {
+        envName = strtok(environ[i], "=");
+        if (strcmp(envName, name) == 0)
+        {
+            envName = strtok(NULL, "=");
+            envValue = envName;
+        }
+        i++;
+    }
+    i = 0;
+    while (envValue[i])
+    {
+        envName = strtok(&envValue[i], ":");
+        if (strcmp(envName, name) == 0)
+        {
+            envName = strtok(NULL, ":");
+            envValue = envName;
+        }
+        i++;
+    printf("%d %s\n", i, envValue);
+    }
+    return (NULL);
+}
+
+int main(void)
+{
+    printf("%s\n", _getenv("PATH"));
+    return (0);
+}
